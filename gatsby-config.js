b2fe0d122f6ccdcb35686c7b53d13d0fa75ce3db
Rita Bradley <contact@ritabradley.com>
2020-07-05 21:28:02 -0700

    chore: 🧹 - Add plugins and dependencies - Configure plugins - Add parameters to .prettierrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,91 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const path = require(`path`)
+
+require(`dotenv`).config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+    plugins: [
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-postcss`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: path.join(__dirname, `src`, `images`),
+            },
+        },
+        {
+            resolve: `gatsby-plugin-page-creator`,
+            options: {
+                path: `${__dirname}/src/templates`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Rita Bradley | Web Developer`,
+                short_name: `RBradley`,
+                start_url: `/`,
+                background_color: `#0A100D`,
+                theme_color: `#A93541`,
+                display: `standalone`,
+                icon: `src/images/favicon.png`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-offline`,
+            options: {
+                precachePages: [`/about/`, `/blog/`, `/projects/*`],
+            },
+        },
+        {
+            resolve: 'gatsby-source-graphql',
+            options: {
+                // Arbitrary name for the remote schema Query type
+                typeName: 'GraphCMS',
+                // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+                fieldName: 'gcms',
+                // Url to query from
+                url:
+                    'https://api-us-west-2.graphcms.com/v2/ckc971p6o06ew01za8nzg93ha/master',
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Quicksand', 'Montserrat'],
+                },
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ['/preview/**', '/do-not-track/me/too/'],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Enables Google Optimize using your container Id
+                optimizeId: process.env.GOOGLE_OPTIMIZE_TRACKING_ID,
+                // Defers execution of google analytics script after page load
+                defer: false,
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: 'example.com',
+            },
+        },
+    ],
 }
