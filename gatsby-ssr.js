c3383233ea76588efaa99095a041eb916876e67e
Rita Bradley <contact@ritabradley.com>
2020-07-08 13:19:59 -0700

    feat: Add footer
    - Add Font Awesome brand icons for social links

---
+++ gatsby-ssr.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <script
+            key="fontawesome"
+            src="https://kit.fontawesome.com/afd1563109.js"
+            crossorigin="anonymous"
+        />,
+    ])
+}
