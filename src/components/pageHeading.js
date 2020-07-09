2c30f0f5e4e853a4e1a9b680f51a19bd05cbe415
Rita Bradley <contact@ritabradley.com>
2020-07-09 15:01:17 -0700

    feat: Create a component for the page title and description - No need for repeating code from page to page - Pass page title and description through props - Have a reusable component

---
+++ src/components/pageHeading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PageHeading = ({ title, description }) => {
+    return (
+        <div className="text-center">
+            <h1 className="font-display text-main-accent laptop:text-4xl laptop:leading-9 text-2xl font-bold leading-10 tracking-wide">
+                {title}
+            </h1>
+            <p className="text-main-text laptop:mt-3 max-w-2xl mx-auto mt-4 text-xl leading-7">
+                {description}
+            </p>
+        </div>
+    )
+}
+
+export default PageHeading
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import SEO from '../components/seo'
+import PageHeading from '../components/pageHeading'
 
 const Blog = () => {
-    return <Layout>Blog</Layout>
+    return (
+        <Layout>
+            <SEO
+                title="Blog"
+                description="Get a glimpse into my random thoughts, projects, struggles (which I overcome) to understand this amazing profession (hobby) of mine."
+                keywords={[
+                    `frontend`,
+                    `web developer`,
+                    `javascript`,
+                    `html`,
+                    `react`,
+                    `css`,
+                    `tailwind css`,
+                    `developer`,
+                    `blog`,
+                    `blogging`,
+                    `posts`,
+                ]}
+            />
+            <PageHeading
+                title="Hot Off My Hamster Wheel"
+                description="Get a glimpse into my random thoughts, projects, struggles (which I overcome) to understand this amazing profession (hobby) of mine."
+            />
+        </Layout>
+    )
 }
 
 export default Blog
+
+export const query = graphql`
+    query {
+        gcms {
+            posts(orderBy: date_DESC) {
+                id
+                title
+                excerpt
+                tags
+                date
+                coverImage {
+                    url
+                }
+            }
+        }
+    }
+`
